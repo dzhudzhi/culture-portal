b87b334f0ec2fcd278124e0bbfd7af1ce3539c17
Anatolii Dzhudzhiev <dzhudzhiev@gmail.com>
2019-01-28 17:03:58 +0300

    fix: build

---
+++ precache-manifest.3ac27ea74b3898fde8c5b2e5d3dc304e.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "425abf173ae14333673785797102db0b",
+    "url": "/culture-portal/static/media/branshteter_1.425abf17.jpg"
+  },
+  {
+    "revision": "6974f9297478983da8a2",
+    "url": "/culture-portal/static/css/main.7c06be4f.chunk.css"
+  },
+  {
+    "revision": "f84496fa22b76cecf94f7a2d9e15c78c",
+    "url": "/culture-portal/static/media/shmerling_title.f84496fa.jpg"
+  },
+  {
+    "revision": "283e29a54bee2ea8ad94",
+    "url": "/culture-portal/static/js/1.283e29a5.chunk.js"
+  },
+  {
+    "revision": "797d2007d3eaafb03907",
+    "url": "/culture-portal/static/js/runtime~main.797d2007.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/culture-portal/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "77c5562cae1c71746251853a5f86573f",
+    "url": "/culture-portal/static/media/developer-anatolii.77c5562c.jpg"
+  },
+  {
+    "revision": "5d1a2dd4134289522d9ea6a9a00e443d",
+    "url": "/culture-portal/static/media/main-page-background.5d1a2dd4.jpg"
+  },
+  {
+    "revision": "207333139a01a154c123156827bad685",
+    "url": "/culture-portal/static/media/barsukov_1.20733313.jpg"
+  },
+  {
+    "revision": "a9db53c036b48877a6fb08c1da2e8569",
+    "url": "/culture-portal/static/media/barsukov_2.a9db53c0.jpg"
+  },
+  {
+    "revision": "aee80e91f9361139248639caec5b3f71",
+    "url": "/culture-portal/static/media/barsukov_3.aee80e91.jpg"
+  },
+  {
+    "revision": "8d59e5bbfc2b48d2b94a321bbfeebb43",
+    "url": "/culture-portal/static/media/barsukov_4.8d59e5bb.jpg"
+  },
+  {
+    "revision": "fd8d5c90bb694409e57fb0021c4ec0c5",
+    "url": "/culture-portal/static/media/barsukov_5.fd8d5c90.jpg"
+  },
+  {
+    "revision": "3ae14005706bbcdf61c5c662a98986c4",
+    "url": "/culture-portal/static/media/barsukov_title.3ae14005.jpg"
+  },
+  {
+    "revision": "09e03e5d63e6725d7257d16a3f20516e",
+    "url": "/culture-portal/static/media/bazan_1.09e03e5d.jpg"
+  },
+  {
+    "revision": "33be3fbd4bc207850f2d40f858f01524",
+    "url": "/culture-portal/static/media/bazan_2.33be3fbd.jpg"
+  },
+  {
+    "revision": "eb149ee4e80b0d64ed55ab30bb68a80f",
+    "url": "/culture-portal/static/media/bazan_3.eb149ee4.jpg"
+  },
+  {
+    "revision": "6ca5a0bafa4b4a068ce2e4b2820912f0",
+    "url": "/culture-portal/static/media/bazan_4.6ca5a0ba.jpg"
+  },
+  {
+    "revision": "c2f844cdedd358ca98f4c43904d7966d",
+    "url": "/culture-portal/static/media/bazan_5.c2f844cd.jpg"
+  },
+  {
+    "revision": "f090cea91db7f34c23864b6f2e5e8278",
+    "url": "/culture-portal/static/media/bazan_title.f090cea9.jpg"
+  },
+  {
+    "revision": "6974f9297478983da8a2",
+    "url": "/culture-portal/static/js/main.6974f929.chunk.js"
+  },
+  {
+    "revision": "004a230ced60d88e2491eb24d6b4e617",
+    "url": "/culture-portal/static/media/branshteter_2.004a230c.jpg"
+  },
+  {
+    "revision": "72d997498efed07d6d4ff34e0bb9da00",
+    "url": "/culture-portal/static/media/branshteter_3.72d99749.jpg"
+  },
+  {
+    "revision": "93019925f448700086831e5a9f3e86d7",
+    "url": "/culture-portal/static/media/branshteter_4.93019925.jpg"
+  },
+  {
+    "revision": "953c44b25d62f3c327975eec0cefac59",
+    "url": "/culture-portal/static/media/branshteter_5.953c44b2.jpg"
+  },
+  {
+    "revision": "d0c2a30ef16e29d8d74580237d542b4e",
+    "url": "/culture-portal/static/media/branshteter_title.d0c2a30e.jpg"
+  },
+  {
+    "revision": "544828f0f28bf9e75cbf43866c542a13",
+    "url": "/culture-portal/static/media/dashkevich_1.544828f0.jpg"
+  },
+  {
+    "revision": "963c49bb9685fc1cdb01804c0eb403d9",
+    "url": "/culture-portal/static/media/dashkevich_2.963c49bb.jpg"
+  },
+  {
+    "revision": "4d3c2c4f4310e4ae43a5facc8f9e60a3",
+    "url": "/culture-portal/static/media/dashkevich_3.4d3c2c4f.jpg"
+  },
+  {
+    "revision": "98f115ea9b3b9f5adeb694789a688be2",
+    "url": "/culture-portal/static/media/dashkevich_4.98f115ea.jpg"
+  },
+  {
+    "revision": "65e4991a8a7c87f30dd74a407e87709e",
+    "url": "/culture-portal/static/media/dashkevich_5.65e4991a.jpg"
+  },
+  {
+    "revision": "f1cd8248bf345f3358a3dbb8c82cfd4b",
+    "url": "/culture-portal/static/media/dashkevich_title.f1cd8248.jpg"
+  },
+  {
+    "revision": "f587b23c359fe98968cc0a672ff22102",
+    "url": "/culture-portal/static/media/iconMarker.f587b23c.svg"
+  },
+  {
+    "revision": "4778cdb56b2b06eace7d0aa3ca018b00",
+    "url": "/culture-portal/static/media/iconMarkerHover.4778cdb5.svg"
+  },
+  {
+    "revision": "65dc0f8488486fbf2781790bd713d697",
+    "url": "/culture-portal/static/media/shmerling_1.65dc0f84.jpg"
+  },
+  {
+    "revision": "4d2a8f6aea9a8d5764c1ed6ef647b64a",
+    "url": "/culture-portal/static/media/shmerling_2.4d2a8f6a.jpg"
+  },
+  {
+    "revision": "62a40fdef94a0e1a48b0748f15ad1d0d",
+    "url": "/culture-portal/static/media/shmerling_3.62a40fde.jpg"
+  },
+  {
+    "revision": "76fe001526bf7b7e49255f83275f9a17",
+    "url": "/culture-portal/static/media/shmerling_4.76fe0015.jpg"
+  },
+  {
+    "revision": "25bbcc657eca72d26e85e5bb19fee7c3",
+    "url": "/culture-portal/static/media/shmerling_5.25bbcc65.jpg"
+  },
+  {
+    "revision": "283e29a54bee2ea8ad94",
+    "url": "/culture-portal/static/css/1.9b99f5da.chunk.css"
+  },
+  {
+    "revision": "24815314bfe098854f13ba4caa6be8c3",
+    "url": "/culture-portal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9018a797cfb480478b56cd37dfc5598c.js"
+  "/culture-portal/precache-manifest.3ac27ea74b3898fde8c5b2e5d3dc304e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/culture-portal/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
